870e976b98d43a28dc0b19dac44dcab4180b1539
bruno <vantofortnite@gmail.com>
2026-06-08 00:34:13 -0500

    feat(network): integracion de gateways, tablas de ruteo y rest api gateway perimetral

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -62,3 +62,56 @@ resource "aws_subnet" "private_6_data_b" {
 
   tags = { Name = "${var.proyecto}-${var.ambiente}-private-subnet-6" }
 }
+
+# 5 Puerta de Enlace a Internet 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "${var.proyecto}-${var.ambiente}-igw" }
+}
+
+# 6 IPs Elásticas para los NAT Gateways
+resource "aws_eip" "nat_1" { domain = "vpc" }
+resource "aws_eip" "nat_2" { domain = "vpc" }
+
+# 7 NAT Gateways (Ubicados en redes públicas para dar salida segura a las privadas)
+resource "aws_nat_gateway" "nat_gw_1" {
+  allocation_id = aws_eip.nat_1.id
+  subnet_id     = aws_subnet.public_1.id
+  tags          = { Name = "${var.proyecto}-${var.ambiente}-nat-gw-1" }
+}
+
+resource "aws_nat_gateway" "nat_gw_2" {
+  allocation_id = aws_eip.nat_2.id
+  subnet_id     = aws_subnet.public_2.id
+  tags          = { Name = "${var.proyecto}-${var.ambiente}-nat-gw-2" }
+}
+
+# 8 Tablas de Enrutamiento Públicas
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "${var.proyecto}-${var.ambiente}-public-rt" }
+}
+
+resource "aws_route_table_association" "pub_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "pub_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 9 Amazon API Gateway 
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "${var.proyecto}-${var.ambiente}-api"
+  description = "Punto de entrada unico para el flujo dinamico de Veltri-Minimarket"
+
+  endpoint_configuration {
+    types = ["REGIONAL"] 
+  }
+}
